84fc7d680176b0ebb46d543f6d6fcfeb42495154
Mesoma10 <Tzmish10@gmail.com>
2023-08-10 20:21:09 -0400

    My first SpringBootProject

---
+++ src/main/java/com/mesomascode/customer/CustomerService.java
@@ -0,0 +1,26 @@
+package com.mesomascode.customer;
+
+import com.mesomascode.exception.ResourceNotFound;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class CustomerService {
+    private final CustomerDao customerDao;
+
+    public CustomerService(CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
+
+    public List<Customer> getAllCustomers(){
+        return customerDao.selectAllCustomers();
+    }
+
+    public Customer getCustomerById(Integer id){
+        return customerDao.selectCustomerById(id)
+                .orElseThrow(() -> new ResourceNotFound(
+                        "customer with id [%s] not found".formatted(id)
+                ));
+    }
+}
