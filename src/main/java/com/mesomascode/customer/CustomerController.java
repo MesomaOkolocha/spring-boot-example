2dbe3530ee876e030b68c146890a5f3372f1e769
Mesoma10 <Tzmish10@gmail.com>
2023-08-15 19:44:39 -0400

    Enhanced CustomerController with CRUD operations and updated request mappings
     Extended the CustomerController class to support CRUD (Create, Read, Update, Delete) operations for managing customer data. The controller now includes the following operations:
    
     1. GET request to retrieve all customers: @GetMapping()
     2. GET request to retrieve a customer by ID: @GetMapping("{customerId}")
     3. POST request to register a new customer: @PostMapping()
     4. DELETE request to delete a customer by ID: @DeleteMapping("{customerId}")
     5. PUT request to update customer information: @PutMapping("{customerId}")
    
     Additionally, request mappings have been updated to use a common base path "api/v1/customers" using the @RequestMapping annotation at the class level. This improves the structure and organization of the API endpoints.

--- src/main/java/com/mesomascode/customer/CustomerController.java
+++ src/main/java/com/mesomascode/customer/CustomerController.java
@@ -1,12 +1,11 @@
 package com.mesomascode.customer;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("api/v1/customers")
 public class CustomerController {
     private final CustomerService customerService;
 
@@ -14,13 +13,27 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @GetMapping("api/v1/customers")
+    @GetMapping()
     public List<Customer> getCustomers() {
         return customerService.getAllCustomers();
     }
 
-    @GetMapping("api/v1/customers/{customerId}")
+    @GetMapping("{customerId}")
     public Customer getCustomerById(@PathVariable("customerId") Integer id) {
         return customerService.getCustomerById(id);
+    }
+
+    @PostMapping()
+    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
+        customerService.addCustomer(customerRegistrationRequest);
+    }
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerService.deleteCustomerById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer( @PathVariable("customerId") Integer id,@RequestBody CustomerUpdateRequest customerUpdateRequest){
+        customerService.updateCustomerById(id,customerUpdateRequest);
     }
 }
