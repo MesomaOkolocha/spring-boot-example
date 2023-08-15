3f1545b56d8b45ae560739c10f91e4a0c65a221b
Mesoma10 <Tzmish10@gmail.com>
2023-08-15 19:47:27 -0400

    Expanded CustomerDao interface with additional methods
    Updated the CustomerDao interface to provide extended functionality for managing customer data in the data access layer. The interface now includes the following methods:
    
    1. void insertCustomer(Customer customer): Inserts a new customer record into the database.
    2. boolean existsPersonWithEmail(String email): Checks if a customer with the given email already exists.
    3. void deleteCustomerById(Integer id): Deletes a customer record by ID.
    4. boolean existsPersonWithId(Integer id): Checks if a customer with the given ID exists.
    5. void UpdateCustomer(Customer customer): Updates an existing customer record.
    
    These new methods enhance the capabilities of the data access layer, allowing for more comprehensive customer data management.

--- src/main/java/com/mesomascode/customer/CustomerDao.java
+++ src/main/java/com/mesomascode/customer/CustomerDao.java
@@ -6,4 +6,11 @@ import java.util.Optional;
 public interface CustomerDao {
     List<Customer> selectAllCustomers();
     Optional<Customer> selectCustomerById(Integer id);
+    void insertCustomer(Customer customer);
+    boolean existsPersonWithEmail(String email);
+    void deleteCustomerById(Integer id);
+
+    boolean existsPersonWithId(Integer id);
+
+    void UpdateCustomer(Customer customer);
 }
