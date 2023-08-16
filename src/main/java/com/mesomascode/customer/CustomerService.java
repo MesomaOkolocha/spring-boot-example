b72d79b6d096b2538a5b0428f4ad12e0f03afd53
Mesoma10 <Tzmish10@gmail.com>
2023-08-15 20:10:42 -0400

    Extended CustomerService with additional methods for adding, deleting, and updating customers
    Enhanced the CustomerService class to provide comprehensive customer management functionalities beyond retrieving customer data. Added the following methods:
    
    1. void addCustomer(CustomerRegistrationRequest customerRegistrationRequest): Validates and adds a new customer using the provided registration request. Checks for duplicate email and inserts the customer into the data store.
    2. void deleteCustomerById(Integer id): Deletes a customer by ID, verifying the customer's existence before performing the deletion.
    3. void updateCustomerById(Integer id, CustomerUpdateRequest customerUpdateRequest): Updates an existing customer's information based on the provided update request. Validates and applies changes to name, email, and age, while ensuring that no duplicate email is used.
    
    These new methods enable complete CRUD (Create, Read, Update, Delete) operations for managing customer data through the service layer.

--- src/main/java/com/mesomascode/customer/CustomerService.java
+++ src/main/java/com/mesomascode/customer/CustomerService.java
@@ -1,15 +1,18 @@
 package com.mesomascode.customer;
 
-import com.mesomascode.exception.ResourceNotFound;
+import com.mesomascode.exception.DuplicateResourceException;
+import com.mesomascode.exception.RequestValidationException;
+import com.mesomascode.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+
 @Service
 public class CustomerService {
     private final CustomerDao customerDao;
 
-    public CustomerService(CustomerDao customerDao) {
+    public CustomerService(@Qualifier("jpa") CustomerDao customerDao) {
         this.customerDao = customerDao;
     }
 
@@ -19,8 +22,60 @@ public class CustomerService {
 
     public Customer getCustomerById(Integer id){
         return customerDao.selectCustomerById(id)
-                .orElseThrow(() -> new ResourceNotFound(
+                .orElseThrow(() -> new ResourceNotFoundException(
                         "customer with id [%s] not found".formatted(id)
                 ));
     }
+
+    public void addCustomer(CustomerRegistrationRequest customerRegistrationRequest){
+        //check if email exists
+        String email = customerRegistrationRequest.email();
+        if (customerDao.existsPersonWithEmail(email)){
+            throw new DuplicateResourceException("email already taken");
+        }
+        Customer customer = new Customer(customerRegistrationRequest.name(),
+                customerRegistrationRequest.email(), customerRegistrationRequest.age());
+        //add
+        customerDao.insertCustomer(customer);
+    }
+
+    public void deleteCustomerById(Integer id){
+        //check if id exists
+        if (!customerDao.existsPersonWithId(id)){
+            throw new ResourceNotFoundException(
+                    "customer with id [%s] not found".formatted(id)
+            );
+        }
+        //delete
+        customerDao.deleteCustomerById(id);
+    }
+
+    public void updateCustomerById(Integer id,CustomerUpdateRequest customerUpdateRequest){
+        Customer existingCustomer = getCustomerById(id);
+        boolean changes = false;
+        String name = customerUpdateRequest.name();
+        String email = customerUpdateRequest.email();
+        Integer age = customerUpdateRequest.age();
+        if(name != null && !name.equals(existingCustomer.getName())){
+            existingCustomer.setName(name);
+            changes = true;
+        }
+        if(age != null && !age.equals(existingCustomer.getAge())){
+            existingCustomer.setAge(age);
+            changes = true;
+        }
+        if(email != null && !email.equals(existingCustomer.getEmail())){
+            if (customerDao.existsPersonWithEmail(email)){
+                throw new DuplicateResourceException("email already taken");
+            }
+            existingCustomer.setEmail(email);
+            changes = true;
+        }
+        if(!changes){
+            throw new RequestValidationException("no data changes found");
+        }
+        customerDao.UpdateCustomer(existingCustomer);
+    }
+
+
 }
